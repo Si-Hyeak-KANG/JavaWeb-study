7ec7f9b98f6101d3901e6e765509b6882135464b
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-05-29 17:23:57 +0900

    no message

--- pro06/src/sec01/ex01/LoginServlet.java
+++ pro06/src/sec01/ex01/LoginServlet.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/login")
+//@WebServlet("/login3")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
