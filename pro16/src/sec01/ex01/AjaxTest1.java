7196b9ca2c8a0f8d627192ffa9495ed8663f3310
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-07-15 22:49:12 +0900

    제이쿼리 Ajax 사용

---
+++ pro16/src/sec01/ex01/AjaxTest1.java
@@ -0,0 +1,40 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxTest1
+ */
+@WebServlet("/ajaxTest1")
+public class AjaxTest1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+	
+		String param = request.getParameter("param");
+		System.out.println("param = " + param);
+		
+		PrintWriter writer = response.getWriter();
+		writer.print("안녕하세요! 서버입니다.");
+	}
+
+}
