7afff71257be53ce2a2046f447f5dd72b7a44ed2
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-07-24 16:08:01 +0900

    모델2 기반 회원 정보 조회 기능 실습

---
+++ pro17/src/sec01/ex01/MemberDAO.java
@@ -0,0 +1,72 @@
+package sec01.ex01;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private DataSource dataFactory;
+	
+	public MemberDAO() {
+		
+		try {
+			System.out.println("DAO »ý¼º");
+			
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
+	public List<MemberVO> listMembers() {
+		
+		List<MemberVO> list = new ArrayList();
+		
+		try {
+			conn = dataFactory.getConnection();
+			String query = "select * from t_member order by joinDate desc";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				
+				MemberVO vo = new MemberVO(id,pwd,name,email,joinDate);
+				
+				list.add(vo);
+			}
+			
+			rs.close();
+			pstmt.close();
+			conn.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+	
+	public void addMember() {
+		
+	}
+	
+}
