fe2b26c4b6e50f747c004f1e9b3871ee11bef378
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-07-24 16:07:36 +0900

    jdk, tomcat 버전 변동

--- pro07/src/sec01/ex01/MemberServlet.java
+++ pro07/src/sec01/ex01/MemberServlet.java
@@ -5,18 +5,20 @@ import java.io.PrintWriter;
 import java.sql.Date;
 import java.util.List;
 
-import jakarta.servlet.Servlet;
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 
 /**
  * Servlet implementation class MemberServlet
  */
-//@WebServlet("/member")
+@WebServlet("/member77")
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
