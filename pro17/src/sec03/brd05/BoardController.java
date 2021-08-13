4984e1656a41ddb1d26f558e37e50d1ce5d85725
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-08-13 22:40:17 +0900

    글 상세 기능 구현(선택 글 수정)

--- pro17/src/sec03/brd05/BoardController.java
+++ pro17/src/sec03/brd05/BoardController.java
@@ -1,7 +1,10 @@
 package sec03.brd05;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +15,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
 
 import sec03.brd02.BoardService;
 
@@ -82,14 +90,93 @@ public class BoardController extends HttpServlet {
 				String content = articleMap.get("content");
 				String imageFileName = articleMap.get("imageFileName");
 				
+				articleVO.setParentNO(0);
+				articleVO.setId("kang");
+				articleVO.setTitle(title);
+				articleVO.setContent(content);
+				articleVO.setImageFileName(imageFileName);
 				
+				articleNO = boardService.addArticle(articleVO);
 				
+				if(imageFileName != null && imageFileName.length() != 0 ) {
+					
+					File srcFile = new File(ARTICLE_IMAGE_REPO + "\\temp\\" + imageFileName);
+					File destdir = new File(ARTICLE_IMAGE_REPO + "\\" +articleNO);
+					destdir.mkdirs();
+					FileUtils.moveFileToDirectory(srcFile, destdir,true);
+					srcFile.delete();
+				}
+	
+				PrintWriter pw = response.getWriter();
+				pw.print("<script>"
+						+ " alert('새글을 작성했습니다.');"
+						+ "location.href='"
+						+ request.getContextPath()
+						+ "/board/listArticles.do';"
+						+ "</script>"
+				);
+				
+				return;
+			} else if (action.equals("/viewArticle.do")) {
+				
+				String articleNO = request.getParameter("articleNO");
+				articleVO = boardService.viewArticle(Integer.parseInt(articleNO));
+				request.setAttribute("article", articleVO);
+				nextPage="/board04/viewArticle.jsp";
 			}
+				
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private Map<String, String> upload(HttpServletRequest request, HttpServletResponse response) {
+
+		Map<String,String> articleMap = new HashMap<String,String>();
+		String encoding = "utf-8";
 		
-		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
-		dispatch.forward(request, response);
+		File currentDirPath = new File(ARTICLE_IMAGE_REPO);
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setRepository(currentDirPath);
+		factory.setSizeThreshold(1024 * 1024);
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		
+		try {
+			
+			List items = upload.parseRequest(request);
+			
+			for(int i = 0; i < items.size(); i++) {
+				
+				FileItem fileItem = (FileItem) items.get(i);
+				if(fileItem.isFormField()) {
+					System.out.println(fileItem.getFieldName() + "=" + fileItem.getString(encoding));
+					articleMap.put(fileItem.getFieldName(), fileItem.getString(encoding));
+				} else {
+					System.out.println("파라미터 명: " + fileItem.getFieldName());
+					System.out.println("파일크기: " + fileItem.getSize() + "bytes");
+					
+					if(fileItem.getSize() > 0) {
+						int idx = fileItem.getName().lastIndexOf("\\");
+						
+						if(idx == -1) {
+							idx = fileItem.getName().lastIndexOf("/");
+						}
+						
+						String fileName = fileItem.getName().substring(idx+1);
+						System.out.println("파일이름: " + fileName);
+						
+						articleMap.put(fileItem.getFieldName(), fileName);
+						
+						File uploadFile = new File(currentDirPath + "\\" + fileName);
+						fileItem.write(uploadFile);
+					} // end if
+				} // end if
+			} // end for
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return articleMap;
 	}
 }
