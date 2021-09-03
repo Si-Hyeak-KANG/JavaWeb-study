a5ed2e76f696d8748bfa7f76f8f3cce7f5a69cb4
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-09-03 23:37:24 +0900

    게시글 페이징 기능

--- pro17/src/sec03/brd08/BoardController.java
+++ pro17/src/sec03/brd08/BoardController.java
@@ -1,6 +1,8 @@
 package sec03.brd08;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,6 +15,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.FileUtils;
 
 import sec03.brd05.ArticleVO;
 import sec03.brd06.BoardService;
@@ -59,43 +63,46 @@ public class BoardController extends HttpServlet {
 		System.out.println("action: " + action);
 		
 		try {
+
 			List<ArticleVO> articlesList = new ArrayList<ArticleVO>();
 			
-			if(action==null) {
+			if (action==null) {
 				String _section = request.getParameter("section");
 				String _pageNum = request.getParameter("pageNum");
 				
-				int section = Integer.parseInt((_section==null)? "1": _section);
-				int pageNum = Integer.parseInt((_pageNum==null)? "1": _pageNum);
+				int section = Integer.parseInt((_section==null)? "1" : _section);
+				int pageNum = Integer.parseInt((_pageNum==null)? "1" : _pageNum);
 				
-				Map<String, Integer> pagingMap = new HashMap<String,Integer>();
+				Map<String, Integer> pagingMap = new HashMap<String, Integer>();
 				pagingMap.put("section", section);
 				pagingMap.put("pageNum", pageNum);
+				
 				Map articlesMap = boardService.listArticles(pagingMap);
 				articlesMap.put("section", section);
 				articlesMap.put("pageNum", pageNum);
 				request.setAttribute("articlesMap",articlesMap);
 				nextPage = "/board07/listArticles.jsp";
-			} else if (action.equals("/listArticles.do")) {
+			} else if (action.equals("/listArticles.do"))) {
 				String _section = request.getParameter("section");
 				String _pageNum = request.getParameter("pageNum");
 				
-				int section = Integer.parseInt((_section==null)? "1": _section);
-				int pageNum = Integer.parseInt((_pageNum==null)? "1": _pageNum);
+				int section = Integer.parseInt((_section==null)? "1" : _section);
+				int pageNum = Integer.parseInt((_pageNum==null)? "1" : _pageNum);
 				
-				Map<String, Integer> pagingMap = new HashMap<String, Integer>();
+				Map<String,Integer> pagingMap = new HashMap<String,Integer>();
 				pagingMap.put("section", section);
 				pagingMap.put("pageNum", pageNum);
+				
 				Map articlesMap = boardService.listArticles(pagingMap);
-				articlesMap.put("section", section);
-				articlesMap.put("pageNum", pageNum);
+				articlesMap.put("section",section);
+				articlesMap.put("pageNum",pageNum);
 				request.setAttribute("articlesMap", articlesMap);
 				nextPage = "/board07/listArticles.jsp";
 			} else if (action.equals("/articleForm.do")) {
 				nextPage = "/board07/articleForm.jsp";
-			} else if (action.equals("addArticle.do")) {
-				int articleNO = 0;
-				Map<String, String> articleMap = upload(request, response);
+			} else if (action.equals("/addArticle.do")) {
+				int articleNO=0;
+				Map<String,String> articleMap = upload(request,response);
 				String title = articleMap.get("title");
 				String content = articleMap.get("content");
 				String imageFileName = articleMap.get("imageFileName");
@@ -105,9 +112,64 @@ public class BoardController extends HttpServlet {
 				articleVO.setTitle(title);
 				articleVO.setContent(content);
 				articleVO.setImageFileName(imageFileName);
-				articleNO = boardService.addArticle(articleVO);
+				articleNO = boardservice.addArticle(articleVO);
+				
+				if (imageFileName != null && imageFileName.length() != 0) {
+					File srcFile = new File(ARTICLE_IMAGE_REPO + "\\" + "temp" + "\\" + imageFileName);
+					File destDir = new File(ARTICLE_IMAGE_REPO + "\\" + articleNO);
+					destDir.mkdirs();
+					FileUtils.moveFileToDirectory(srcFile, destDir, true);
+				}
+				PrintWriter pw = response.getWriter();
+				pw.print("<script>" + " alert('새글을 추가했습니다.');" + " location.href='" + request.getContextPath()+"/board/listArticles.do';" + "</script>");
+				
+				return;
+				
+			} else if (action.equals("/viewArticle.do")) {
+				String articleNO = request.getParameter("articleNO");
+				articleVO = boardService.viewArticle(Integer.parseInt(articleNO));
+				request.setAttribute("article", articleVO);
+				nextPage = "/board07/viewArticle.jsp";
+				
+			} else if (action.equals("/modArticle.do")) {
+				Map<String,String> articleMap = upload(request,response);
+				int articleNO =Integer.parseInt(articleMap.get("articleNO"));
+				String title = articleMap.get("title");
+				String content = articleMap.get("content");
+				String imageFileName = articleMap.get("imageFileName");
+						
+				articleVO.setArticleNO(articleNO);
+				articleVO.setTitle(title);
+				articleVO.setContent(content);
+				articleVO.setImageFileName(imageFileName);
+				boardService.modArticle(articleVO);
+				
+				if (imageFileName != null && imageFileName.length() != 0) {
+					
+					String originalFileName = articleMap.get("originalFileName");
+					
+					File srcFile = new File(ARTICLE_IMAGE_REPO + "\\" + "temp" +"\\" + imageFileName);
+					File destDir = new File(ARTICLE_IMAGE_REPO + "\\" + articleNO);
+					destDir.mkdirs();
+					FileUtils.moveFileToDirectory(srcFile, destDir, true);
+					
+					File oldFile = new File(ARTICLE_IMAGE_REPO + "\\" + articleNO + "\\" + originalFileName);
+					oldFile.delete();
+					
+				}
+				
+				PrintWriter pw = response.getWriter();
+				pw.print("<script>"+ " alert('글을 수정했습니다.');" + " location.href='" + request.getContextPath() + "/board/viewArticle.do?articleNO=" + articleNO + "';" + " </script>");
+				
+				return;
+			} else if (action.equals("/removeArticle.do" )) {
+				int articleNO = Integer.parseInt(request.getParameter("articleNO"));
+				List<Integer> articleNOList
 			}
 			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 
