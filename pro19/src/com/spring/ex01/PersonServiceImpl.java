5a40dad00c49a464f09c5f70b6bbbeb5b3de7344
Si-Hyeak-KANG <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2021-11-13 22:37:39 +0900

    Spring 의존성 주입 실습

---
+++ pro19/src/com/spring/ex01/PersonServiceImpl.java
@@ -0,0 +1,19 @@
+package com.spring.ex01;
+
+public class PersonServiceImpl implements PersonService {
+
+	private String name;
+	private int age;
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public void sayHello() {
+		System.out.println("이름: " + name);
+		System.out.println("나이: " + age);
+	}
+}
