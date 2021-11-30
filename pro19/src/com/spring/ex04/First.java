c321875f8da61f69a5e2b9038b828c7755ed41fa
Si-Hyeak-Kang <zlcls456@gamilcom>
2021-11-30 23:43:54 +0900

    빈 태그에 lazy-init 속성 사용 실습

---
+++ pro19/src/com/spring/ex04/First.java
@@ -0,0 +1,7 @@
+package com.spring.ex04;
+
+public class First {
+	public First() {
+		System.out.println("First 생성자 호출");
+	}
+}
---
+++ pro19/src/com/spring/ex04/Third.java
@@ -0,0 +1,8 @@
+package com.spring.ex04;
+
+public class Third {
+
+	public Third() {
+		System.out.println("Third 생성자 호출");
+	}
+}
